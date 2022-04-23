992de36ea9dc4c5976d201713e6d671a4b6e2f2d
Rusty cat <tsukiroku@yahoo.com>
2022-04-23 18:42:37 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
+import { BASE_URL, token } from "./config.js";
 import { Octokit } from "@octokit/core";
-import { token, BASE_URL, IFollowerUser, R, log } from "./dd.js";
 import axios from "axios";
 
+interface IFollowerUser { 
+    login: string; 
+    id: number; 
+};
+
+interface ILogger { 
+    message: string; 
+    _type: 'info' | 'error'; 
+};
+
+type R<T> = { readonly [k in keyof T]?: T[k] };
+
+const block = async (method: string, user: IFollowerUser) => {   
+    await new Octokit({ auth: token }).request(`${method} /user/blocks/${user?.login}`).catch((err) => {
+        log({ message: `(${method}) ${err}`, _type: "error" });
+    });
+}
+
+const log = (d: ILogger) => console.log(`[${d._type}]: ${d.message}`);
+
 (async () => {
-    await axios.get<R<IFollowerUser[]>>(BASE_URL).then((res) => {
+    await axios.get<R<Array<IFollowerUser>>>(BASE_URL).then((res) => {
         res.data.forEach(async (user) => {
-            log({ message: `name: ${user?.login}, id: ${user?.id}`, _type: "info" });
-
-            const octokit = new Octokit({ auth: token });
-
-            ["PUT", "DELETE"].forEach(async (m) => {
-                await octokit.request(`${m} /user/blocks/${user?.login}`).catch((err) => {
-                    log({ message: `(${m}) ${err}`, _type: "error" });
-                });
-            });
+            await block('PUT', user!);
+            await block('DELETE', user!);
 
             log({ message: `OK (${user?.login})`, _type: "info" });
         });
